ddaa33b8efeb1f052bd979c164d0f964820abea1
Jesse Donat <donatj@gmail.com>
2017-04-24 15:24:43 -0500

    Adds Sounds constants

---
+++ src/Sounds.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace donatj\Pushover;
+
+class Sounds {
+	const PUSHOVER      = 'pushover'; // Pushover (default)
+	const BIKE          = 'bike'; // Bike
+	const BUGLE         = 'bugle'; // Bugle
+	const CASH_REGISTER = 'cashregister'; // Cash Register
+	const CLASSICAL     = 'classical'; // Classical
+	const COSMIC        = 'cosmic'; // Cosmic
+	const FALLING       = 'falling'; // Falling
+	const GAMELAN       = 'gamelan'; // Gamelan
+	const INCOMING      = 'incoming'; // Incoming
+	const INTERMISSION  = 'intermission'; // Intermission
+	const MAGIC         = 'magic'; // Magic
+	const MECHANICAL    = 'mechanical'; // Mechanical
+	const PIANO_BAR     = 'pianobar'; // Piano Bar
+	const SIREN         = 'siren'; // Siren
+	const SPACE_ALARM   = 'spacealarm'; // Space Alarm
+	const TUGBOAT       = 'tugboat'; // Tug Boat
+	const ALIEN         = 'alien'; // Alien Alarm (long)
+	const CLIMB         = 'climb'; // Climb (long)
+	const PERSISTENT    = 'persistent'; // Persistent (long)
+	const PUSHOVER_ECHO = 'echo'; // Pushover Echo (long)
+	const UP_DOWN       = 'updown'; // Up Down (long)
+	const NONE          = 'none'; // None (silent)
+}
